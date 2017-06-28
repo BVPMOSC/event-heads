ea5db5fdb27da056b4ad289c25e4b143f42ae106
aniket965 <aniket965.as@gmail.com>
2017-06-28 15:22:41 +0530

    added changes!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-var server = app.listen(5000 || process.env.PORT, function () {
+var server = app.listen(process.env.PORT, function () {
     console.log('running on port 5000');
 });
